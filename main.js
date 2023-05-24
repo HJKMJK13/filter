593078c5ce64f0a5a82a97708e8b5c4c9d188b63
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-24 20:23:02 +0500

    endi imgni chaqirib olamiz

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fileInputElement=document.querySelector(".file-input")
 const chooseImgBtnElement=document.querySelector(".choose-img")
+const previewImgElement=document.querySelector(".preview-img img")
 
 const loadImage=()=>{
     let file=fileInputElement.files[0]
