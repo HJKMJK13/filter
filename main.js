9607b22533fa1ece2335e7ab3b03637572dd4aa9
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-24 20:21:18 +0500

    elementlarni chaqirib olishdan boshlimiz

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const 
+const fileInputElement=document.querySelector(".file-input")
+const chooseImgBtnElement=document.querySelector(".choose-img")
+
+const loadImage=()=>{
+    let file=fileInputElement.files[0]
+    console.log(file)
+    if(!file)return
+}
+fileInputElement.addEventListener("change",loadImage)
+chooseImgBtnElement.addEventListener("click",()=>fileInputElement.click())
