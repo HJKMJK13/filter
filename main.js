804889619cb89356bd82e71ab8af1a64775cbb32
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-24 20:50:18 +0500

    buttonlarni active qildik

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const fileInputElement=document.querySelector(".file-input")
 const chooseImgBtnElement=document.querySelector(".choose-img")
 const previewImgElement=document.querySelector(".preview-img img")
+const filterElement=document.querySelectorAll(".filter button")
 
 const loadImage=()=>{
     let file=fileInputElement.files[0]
     console.log(file)
     if(!file)return
     previewImgElement.src=URL.createObjectURL(file)
+    previewImgElement.addEventListener("load",()=>{
+        document.querySelector(".container").classList.remove("disabled")
+    })
 }
+
+filterElement.forEach(Option=>{
+    Option.addEventListener("click",()=>{
+        document.querySelector(".filter .active").classList.remove("active")
+        Option.classList.add("active")
+    })
+})
 fileInputElement.addEventListener("change",loadImage)
 chooseImgBtnElement.addEventListener("click",()=>fileInputElement.click())
