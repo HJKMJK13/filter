68bff040ab6c56db2c578439d41bbf7799df7e0e
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-24 20:55:53 +0500

    filterElementNameni ishlatdik endi qaysi buttonni bosganimizda osha button  malumoti chiqadi

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const fileInputElement=document.querySelector(".file-input")
 const chooseImgBtnElement=document.querySelector(".choose-img")
 const previewImgElement=document.querySelector(".preview-img img")
 const filterElement=document.querySelectorAll(".filter button")
+const filterNameElement=document.querySelector(".filter-info .name")
 
 const loadImage=()=>{
     let file=fileInputElement.files[0]
@@ -17,6 +18,7 @@ filterElement.forEach(Option=>{
     Option.addEventListener("click",()=>{
         document.querySelector(".filter .active").classList.remove("active")
         Option.classList.add("active")
+        filterNameElement.textContent=Option.textContent
     })
 })
 fileInputElement.addEventListener("change",loadImage)
