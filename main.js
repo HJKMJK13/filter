2841e8264107649e7f9f5073af6bc401364f8dbd
HJKMJK13 <norboyevkarimjon06@gmail.com>
2023-05-24 21:22:50 +0500

    ranglarni o'zgartirish navbati keldi

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const filterNameElement=document.querySelector(".filter-info .name")
 const filterSliderElement=document.querySelector(".slider input")
 const filterValueElement=document.querySelector(".filter-info .value")
 
+let brightness=100,saturation=100,inversion=0,grayscale=0;
+
 const loadImage=()=>{
     let file=fileInputElement.files[0]
     console.log(file)
@@ -21,11 +23,40 @@ filterElement.forEach(Option=>{
         document.querySelector(".filter .active").classList.remove("active")
         Option.classList.add("active")
         filterNameElement.textContent=Option.textContent
+
+        if(Option.id ==="brightness"){
+            filterSliderElement.max="200"
+            filterSliderElement.value=brightness
+            filterValueElement.textContent=`${brightness}%`
+        }else if(Option.id ==="saturation"){
+            filterSliderElement.max="200"
+            filterSliderElement.value=saturation
+            filterValueElement.textContent=`${saturation}%`
+        }else if(Option.id ==="inversion"){
+            filterSliderElement.max="100"
+            filterSliderElement.value=inversion
+            filterValueElement.textContent=`${inversion}%`
+        }else{
+            filterSliderElement.max="100"
+            filterSliderElement.value=grayscale
+            filterValueElement.textContent=`${grayscale}%`
+        }
     })
 })
 
 const updateFilter=()=>{
     filterValueElement.textContent=`${filterSliderElement.value}%`
+    const selectFilterELement=document.querySelector(".filter .active")
+
+    if(selectFilterELement.id === "brightness"){
+        brightness=filterSliderElement.value
+    }else if(selectFilterELement.id ==="saturation"){
+        saturation=filterSliderElement.value
+    }else if(selectFilterELement.id ==="inversion"){
+        inversion=filterSliderElement.value
+    }else{
+        grayscale=filterSliderElement.value
+    }
 }
 fileInputElement.addEventListener("change",loadImage)
 chooseImgBtnElement.addEventListener("click",()=>fileInputElement.click())
